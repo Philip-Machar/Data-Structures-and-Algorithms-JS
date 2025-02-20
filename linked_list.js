09d89b842367f3dadd4bf78e57dd82573eaaff15
Philip-Machar <philipmachar788@gmail.com>
2025-02-20 14:05:22 +0300

    Linked list

---
+++ linked_list.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    };
+};
+
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    };
+
+    append(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = newNode
+        };
+
+        this.size++;
+    };
+};
+
+
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+
+console.log(list);
+
